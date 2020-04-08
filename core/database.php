1c0318052e3f3701afe54c145f2003e8e67ccbb7
Khadim Diaw <khadim.diaw@gmail.com>
2020-04-08 13:38:42 +0000

    classe database ok

--- core/database.php
+++ core/database.php
@@ -1,23 +1,19 @@
 <?php
     class Database{
 
-        private $name;
-        private $user;
-        private $pass;
-        private $host;
-        private $pdo;
+        public static $pdo;
 
-        public function __construct($name, $user = 'root', $pass = '', $host = 'localhost'){
-            $this->name = $name;
-            $this->user = $user;
-            $this->pass = $pass;
-            $this->host = $host;
+        public function __construct(){
+           
         }
 
-        private function getPDO(){
-            $pdo = new PDO('mysql:dbname=gestock;host=localhost', 'root', '');
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $this->pdo = $pdo;
-            return $pdo;
+        public static function getPDO(){
+            if (self::$pdo === null){
+                self::$pdo = new pdo('mysql:host=localhost; dbname=bibliotheque', 'root', '');
+                self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                return self::$pdo; 
+            }else{
+                return self::$pdo;
+            }
         }
     }
