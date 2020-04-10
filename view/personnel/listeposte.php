663b59d193743ef5a989d41dba3e49121cbe03a6
salimata wade gueye <neoreload82@gmail.com>
2020-04-10 13:40:19 +0000

    ajout button modifier

--- view/personnel/listeposte.php
+++ view/personnel/listeposte.php
@@ -8,7 +8,17 @@
        <?php foreach($postes as $poste): ?>
         <tr>
             <td><?= $poste->nom ?></td>
-            <td></td>
+            <td>
+    
+            <input type="hidden" name="action" value="modifier">
+            <input type="submit" value="modifier" class="btn btn-success">     
+            
+
+            
+            <input type="hidden" name="action" value="supprimer">
+            <input type="submit" value="supprimer" class="btn btn-success">
+
+            </td>
         </tr>
         <?php endforeach ; ?>
    </table>
