818dabdc4ebe80a96eedbec185d0c95b112946c6
Awa SENE <asene453@gmail.com>
2020-04-10 18:21:44 +0200

    ajoute modification listnomarticle

--- view/nomarcticle/listnomArticles.php
+++ view/nomarcticle/listnomArticles.php
@@ -1,13 +1,20 @@
 <h1 class="liste">Liste Article</h1>
         <div>
-            <table>
+            <table >
                 <tr>
                     <th>Nom article</th>
                     <th>Action</th>
                 </tr>
                 <tr>
-                    <td>Ordinateur</td>
-                    <td><img src="images\icones\delete"></td>
+                    <td>
+                        <a href=""><button class="btn btn-info"><img src="images\icones\pencil.png">Modifier</button></a>
+                        <a href=""><button class="btn btn-danger"><img src="images\icones\delete.png">Supprimer</button></a>
+                    <td>
                 </tr>
             </table>
         </div>
+        <div class="mt-5">
+            <a href="/gestock/article/ajouter-article"><button class="btn btn-success ml-5"><img src="images\icones\ajout.png">Ajouter</button></a>
+
+        </div>
+        
