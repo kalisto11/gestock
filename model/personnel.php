878c3caa0ef456358a2800d0d067b894bd256f56
Daouda <kontedaouda96@gmail.com>
2020-04-14 19:09:50 +0000

    Modification du model Personnel

--- model/personnel.php
+++ model/personnel.php
@@ -22,10 +22,10 @@
             $this->poste = $personnel['poste'];
         }
         else{
-            $this->id = null;
+            $this->id     = null;
             $this->prenom = null;
-            $this->nom = null;
-            $this->poste = null;
+            $this->nom    = null;
+            $this->poste  = null;
         }
     }
     public function save(){ // fonction d'ajout d'un personnel
@@ -33,40 +33,40 @@
         $req = 'INSERT INTO personnel (prenom, nom, poste) VALUES (:prenom, :nom, :poste)';
         $reponse = $pdo->prepare($req);
         $reponse->execute(array(
-        'prenom' => $this->nom,
-        'nom' => $this->nom,
-        'poste' =>$this->poste
+        'prenom' => $this->prenom,
+        'nom'    => $this->nom,
+        'poste'  => $this->poste
         ));
     }  
     public function update(){ //fonction permettant de modifier les données d'un personnel
         $pdo = Database::getPDO();
-        $req = 'UPDATE poste SET prenom = :nom, nom = :nom, poste = :poste WHERE id = :id';
+        $req = 'UPDATE personnel SET prenom = :prenom, nom = :nom, poste = :poste WHERE id = :id';
         $reponse = $pdo->prepare($req) OR die(print_r($pdo->errorinfo()));
         $resultat = $reponse->execute(array(
         'prenom' => $this->prenom,
-        'nom' =>$this->nom,
-        'poste' =>$this->poste,
-        'id' => $this->id
+        'nom'    => $this->nom,
+        'poste'  => $this->poste,
+        'id'     => $this->id
         ));
     }  
     public function delete(){ //Fonction de suppresssion d'un personnel
         $pdo = Database::getPDO();
-        $req = 'DELETE from personnel WHERE id = ?';
-        $reponse = $pdo->prepare('DELETE from personnel WHERE id = ?');
+        $sup = 'DELETE from personnel WHERE id = ?';
+        $reponse = $pdo->prepare($sup);
         $reponse->execute(array($this->id));
     }
     public static function getlist(){ //Fonction permettant d'obtenir la liste du personnel
         $pdo = Database::getPDO();
-        $req = 'SELECT * from personnel';
-        $reponse = $pdo->query($req);
+        $get = 'SELECT * from personnel';
+        $reponse = $pdo->query($get);
         $personnels = array();
         while ($row = $reponse->fetch()){
             $personnel = new Personnel();
-            $personnel->id = $row['id'];
+            $personnel->id     = $row['id'];
             $personnel->prenom = $row['prenom'];
-            $personnel->nom = $row['nom'];
-            $personnel->poste = $row['poste'];
-            $personnels[] = $personnel;
+            $personnel->nom    = $row['nom'];
+            $personnel->poste  = $row['poste'];
+            $personnels[]      = $personnel;
         }
         return $personnels;
     }
