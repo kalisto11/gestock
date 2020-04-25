30d5304a1991ed90dd44c05b67845363dc8e7ff5
Khadim Diaw <khadim.diaw@gmail.com>
2020-04-25 23:45:36 +0000

    gestion des erreurs

--- controller/articles.php
+++ controller/articles.php
@@ -11,28 +11,27 @@
                 switch ($_POST['operation']){
                     case 'ajouter':
                         $this->traiterArticle($_POST['groupe'], $_POST['article']);
-                        $this->render($this->notification);
                     break;  
 
                     case 'modifier':          
                         $this->traiterArticle($_POST['groupe'], $_POST['article'], $_POST['idArticle']);
-                        $this->render($this->notification);
                     break;
 
                     default:
                     $this->notification = new Notification("danger", "Une erreur s'est produite pendant le traitement des données. Veuillez rééssayer svp.");
                     $this->request->action = 'liste';
-                    $this->render($this->notification);
-                }            
-            }  // fin traitement de la méthode post
+                }  
+                $this->render($this->notification);          
+            }  // fin traitement de la méthode POST
+
             elseif ($this->request->method === 'GET'){ //Si la requete vient d'un lien
                 if ($this->request->action === 'supprimer'){
                     $idArticle = intval($this->request->id);
                     $article = new Article($idArticle);
                     $article->supprime(); 
                     $this->request->action = 'liste';
-                    $notification[] = "L'article a été supprimé avec succès.";
-                    $this->notification = new Notification("success", $notification);
+                    $message[] = "L'article a été supprimé avec succès.";
+                    $this->notification = new Notification("success", $message);
                 }
                 $this->render($this->notification); 
             } // fin traitement de la méthode GET
@@ -40,7 +39,7 @@
          
         public function render($notification = null){
              switch ($this->request->action){
-                // inclure les vues ici selon la valeur de $view
+                // inclure les vues ici selon la valeur de $this->request->action
                 case 'liste':
                      $articles = Article::getList();
                      require_once VIEW . 'article/listarticles.php';
@@ -68,25 +67,43 @@
         public function traiterArticle($groupeArticle, $nomArticle, $id = null){
             $erreurs = false;
             $articles = Article::getList();
-            foreach ($articles as $article){
-                if ($article->nom == $nomArticle){
-                    $erreurs = true;
-                    $notification[] = "Le nom de l'article existe déja. Veuillez choisir un autre nom.";
-                }   
-            }
             if (empty($nomArticle)){
                 $erreurs = true;
-                $notification[] = "Le nom de l'article ne doit pas etre vide.";
+                $message[] = "Le nom de l'article ne doit pas etre vide.";
             }
-
-            if ($erreurs == false){
+            if ($id == null){ // cas ajout
+                foreach ($articles as $article){
+                    if ($article->nom == $nomArticle){
+                        $erreurs = true;
+                        $message[] = "Le nom de l'article existe déja. Veuillez choisir un autre nom.";
+                    }
+                }
+            }
+            else{ // cas modification
+                $noms = array() ;
+                foreach ($articles as $article){
+                    $noms[] = $article->nom;
+                }
+                $article = new Article($id);
+                foreach ($noms as $nom){
+                    if ($nom == $article->nom){
+                        unset($noms[array_search($article->nom, $noms)]);
+                    }
+                }
+                if (in_array($nomArticle, $noms)){
+                    $erreurs = true;
+                    $message[] = "Le nom de l'article existe déja. Veuillez choisir un autre nom.";
+                }
+            }
+                
+            if ($erreurs == false){ // si pas d'erreurs
                 if ($id == null){ // cas ajouter article
                     $article = new Article();
                     $article->nom = strip_tags($nomArticle);   
                     $article->groupe = strip_tags($groupeArticle);                                            
                     $article->ajoutArticle();
-                    $notification[] = "L'article a été ajouté avec succès.";
-                    $this->notification = new Notification("success", $notification);
+                    $message[] = "L'article a été ajouté avec succès.";
+                    $this->notification = new Notification("success", $message);
                     $this->request->action = 'liste';
                 }
                 else{ // cas modifier article
@@ -95,14 +112,14 @@
                     $article->nom = strip_tags($nomArticle);
                     $article->groupe = strip_tags($groupeArticle);
                     $article->modif();
-                    $notification[] = "L'article a été modifié avec succès.";
-                    $this->notification = new Notification("success", $notification);
+                    $message[] = "L'article a été modifié avec succès.";
+                    $this->notification = new Notification("success", $message);
                     $this->request->action = 'liste';
                     $this->request->id = $id;
                 }
             }
             else{ // cas ou il y a des erreurs
-                $this->notification = new Notification("danger", $notification);
+                $this->notification = new Notification("danger", $message);
                 if ($id == null){
                     $this->request->action = 'liste';
                 }
