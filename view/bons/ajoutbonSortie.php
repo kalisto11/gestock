7300a113886913c7967727171a9e05063647acbb
Khadim Diaw <khadim.diaw@gmail.com>
2020-04-28 13:47:59 +0000

    fix liste artcile formulaire bon de sortie

--- view/bons/ajoutbonSortie.php
+++ view/bons/ajoutbonSortie.php
@@ -1,36 +1,39 @@
-<h1 class="text-center">Ajouter un Bon de Sortie</h1>
+<h1 class="text-center">Ajouter un bon de sortie</h1>
 <div class="container"> 
     <form method="post" action="/gestock/bonssortie/traitement-bonsortie">
       <div class="form-group">
         <label for="reference">Référence</label>
         <input type="text" name="reference" id="reference" class="form-control">
       </div>
-        <?php for ($i = 0; $i < 10; $i++) :?>
-        <div class="row">
-          <div class="col">
-            <div class="form-groupe">
-               <label for="article1">Article </label>
-               <select name="article<?php $i+1 ?>" id="article" class="form-control">
-               <?php foreach($articles as $article): ?>
-               <option value="null">---------------------------</option>
-                <?php endforeach ; ?>
-              </select>
-            </div> 
+      <?php for ($i = 0; $i < 10; $i++) :?>
+      <div class="row">
+        <div class="col">
+          <div class="form-group">
+            <label for="article1">Article </label>
+            <select name="article<?php $i+1 ?>" id="article" class="form-control">
+              <option value="null">-----------------------------------------------</option>
+              <?php foreach($articles as $article): ?>
+              <option value="<?= $article->id ?>"><?= $article->nom ?></option>
+              <?php endforeach ; ?>
+            </select>
+          </div> 
+        </div>
+        <div class="col">
+          <div class="form-group">
+            <label for="quantite">Quantité</label>
+            <input type="number" name="quantite" id="quantite" class="form-control">
           </div>
-         <div class="col">
-              <label for="quantite">Quantité 1</label>
-              <input type="number" name="quantite" id="quantite" class="form-control">
-          </div>
-        </div> 
-        <?php endfor ; ?>
+        </div>
+      </div> 
+      <?php endfor ; ?>
       <div class="form-group mt-3">
         <label for="beneficiaire">Bénéficiaire</label>
         <select name="beneficiaire" id="beneficiaire" class="form-control">
-                <option value="null">----------------------------------</option>
-                <?php foreach($personnels as $personnel): ?>
-                <option value="<?= $personnel->id ?>">  <?= $personnel->prenom?>  <?= $personnel->nom ?></option>
-                <?php endforeach ; ?>
-                </select>
+          <option value="null">----------------------------------</option>
+          <?php foreach($personnels as $personnel): ?>
+          <option value="<?= $personnel->id ?>">  <?= $personnel->prenom?>  <?= $personnel->nom ?></option>
+          <?php endforeach ; ?>
+        </select>
       </div>
   
       <input type="hidden" name="operation" value="ajouter">
