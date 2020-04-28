0c195e27e951c8cf8da750bb174ba30668fd2ae2
salimata wade gueye <neoreload82@gmail.com>
2020-04-28 12:16:40 +0000

    modif bon de sortie

--- view/bons/listbonentree.php
+++ view/bons/listbonentree.php
@@ -1,4 +1,5 @@
 <?php require VIEW . 'infos/notifications.php'; ?>
+
 <h2 class="mt-5 text-center">Liste des Bons d'entrée</h2> 
 <div>
     <table class="table table-striped table-bordered table-hover">
