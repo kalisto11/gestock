637be700080540cc66e4f6488e04c19388e289d5
Khadim Diaw <khadim.diaw@gmail.com>
2020-04-28 12:28:16 +0000

    modification formulaire d'ajout de bon d'entree

--- view/bons/ajoutbonentree.php
+++ view/bons/ajoutbonentree.php
@@ -11,7 +11,7 @@
       <div class="col">
         <div class="form-group">
           <label for="article">Article</label>
-          <select name="article" id="article" class="form-control">
+          <select name="article<?= $i + 1 ?>" id="article" class="form-control">
             <option value="null">----------------------------------</option>
             <?php foreach ($articles as $article) : ?>
             <option value="<?= $article->id ?>"><?= $article->nom ?></option>
