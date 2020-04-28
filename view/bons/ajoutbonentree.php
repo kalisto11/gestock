3adc57c3d959de67ab6d0d8c490db09fc06ce977
Khadim Diaw <khadim.diaw@gmail.com>
2020-04-28 23:26:11 +0000

    fix modif form ajout bon entree

--- view/bons/ajoutbonentree.php
+++ view/bons/ajoutbonentree.php
@@ -1,11 +1,17 @@
 <?php require VIEW . 'infos/notifications.php'; ?>
-<h2 class="text-center">Ajouter un bon d'entree</h2>
+<h2 class="text-center">Ajouter un bon d'entrée</h2>
 <div> 
   <form method="post" action="/gestock/bonsentree/traitement-bonentree">
     <div class="form-group">
       <label for="reference">Référence</label>
       <input type="text" name="reference" id="reference" class="form-control">
     </div>
+
+    <div class="form-group">
+      <label for="fournisseur">Fournisseur</label>
+      <input type="text" name="fournisseur" id="fournisseur" class="form-control">
+    </div>
+
     <?php for ($i = 1; $i <= 10; $i++) : ?>
     <div class="row">
       <div class="col">
@@ -25,15 +31,13 @@
       </div>
     </div>
     <?php endfor ; ?>
-       
-    <div class="form-group">
-      <label for="fournisseur">Fournisseur</label>
-      <input type="text" name="fournisseur" id="fournisseur" class="form-control">
-    </div>
 
     <input type="hidden" name="operation" value="ajouter">
-
-    <input type="submit" value="Ajouter" class="btn btn-success">
-    <a href="/gestock/bonsentree/liste" class="btn btn-danger">Annuler</a>    
+    
+    <div class="mt-5">
+      <input type="submit" value="Ajouter" class="btn btn-success">
+      <a href="/gestock/bonsentree/liste" class="btn btn-danger">Annuler</a>
+    </div>  
+    
   </form>
 </div>
